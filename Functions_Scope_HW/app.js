6244754e7e0f0aedc36e5698b8b1ea12ee53e37f
keyboard keys <keyboard@keyboards-iMac.local>
2022-09-12 18:48:02 -0400

    Commit 1 - Verbal questions

---
+++ Functions_Scope_HW/app.js
@@ -0,0 +1,7 @@
+// What is the difference between a parameter and an argument?
+                // an argument is an object.  A parameter is a variable.  Ie, when multiplying
+                        // (X,Y) x and y are parameters in which you can enter different numbers for the func to multiply
+// Within a function, what is the difference between return and console.log?
+            // return makes a calculation, console.log rewrites a selected statement
+// // What are the implications of the ability of a function to return a value?
+//     functions can compute mathematical calculations
