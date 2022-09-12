807e6e08b9bd5e7030e546ac26c8efdfdca9615f
keyboard keys <keyboard@keyboards-iMac.local>
2022-09-12 19:52:54 -0400

    Commit 4 - Pythagoras

--- Functions_Scope_HW/app.js
+++ Functions_Scope_HW/app.js
@@ -5,3 +5,40 @@
             // return makes a calculation, console.log rewrites a selected statement
 // // What are the implications of the ability of a function to return a value?
 //     functions can compute mathematical calculations
+
+// const checkPalindrome = (string) => {
+
+// if (Radar === radaR) {
+//     console.log("Radar")
+//     }
+
+// console.log(checkPalindrome("Radar"));
+// //  => true
+
+
+//  if ("Borscht" !== "thcsroB") {
+//     console.log("")
+//     }
+// }
+// console.log(checkPalindrome("Radar"));
+//  => true
+// *** look for palindrome method online**********
+
+// array methods, turn num into an array, method to add those
+
+func sumDigits(num) {
+    for (let i = 0; i < num.length; i++) {
+    return sum;
+    }   
+}
+console.log(sumDigits(42));
+
+// function calculateSide(sideA, sideB){
+//     return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+//   }
+  
+//   console.log(calculateSide(8, 6));
+
+
+
+
